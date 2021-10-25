cf4cebe8228047501dbb1b04bc5ea1d0b136cc9f
zidanlr <zlr54117@gmail.com>
2021-10-25 09:42:49 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# tugas 2
+## latihan membuat script phyton sederhana
+###
+<img src= "ss2.png">
---
+++ tugas.py
@@ -0,0 +1,5 @@
+print ("Assalamu'alaikum")
+print ("")
+print ("Let me introduce my self. My name Zidan Lutfi Ramadhan but you can call me Zidan. My NPM is 312110304.I was born in KEBUMEN and i am 18 years old. i am very glad if you want to invit my house in purinirwana residence sukaraya.So, don't forget to call me before with the number 025155120028")
+print ("")
+print ("thank you")
